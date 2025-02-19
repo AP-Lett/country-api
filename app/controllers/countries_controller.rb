fc0fc34892e5ae1487828210d110208c153bc376
AP-Lett <audreana.lett@gmail.com>
2025-02-19 20:12:44 +1000

    All working

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,3 +1,28 @@
 class CountriesController < ApplicationController
-  
+  def index #works
+    countries = Country.all
+    render json: countries
+  end
+
+  def show #works
+    country = Country.find(params[:id])
+    render json: country
+  end
+
+  def create #works
+    country = Country.create(name: params[:name], capital:params[:capital], population: params[:population], demonym: params[:demonym], fun_fact: params[:fun_fact])
+    render json: country
+  end
+
+  def update #works
+    country = Country.find(params[:id])
+    country.update(name: params[:name], capital:params[:capital], population: params[:population], demonym: params[:demonym], fun_fact: params[:fun_fact])
+    render json: country
+  end
+
+  def destroy #works
+    country = Country.find(params[:id])
+    country.destroy
+    render :no_content
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 get '/countries' => 'countries#index'
+get '/countries/:id' => 'countries#show'
+post '/countries' => 'countries#create'
+patch '/countries/:id' => 'countries#update'
+delete '/countries/:id' => 'countries#destroy'
 end
